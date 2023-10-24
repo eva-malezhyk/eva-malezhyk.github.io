9461b0ebd46d8a8efebaca0fde7a4c52f8b182c3
eva-malezhyk <evamal1201@gmail.com>
2023-10-24 11:28:08 -0600

    Terrain

---
+++ 09-terrain/sketch.js
@@ -0,0 +1,53 @@
+// Terrain Generation
+// Oc 23, 2023
+
+let terrain = [];
+let xOffset = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  spawnRectangles();
+}
+
+function draw() {
+  background(220);
+
+  if (keyIsDown(RIGHT_ARROW)){
+     if (xOffset < 5){
+      xOffset += 20; 
+     }
+    
+
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    if (xOffset > 5){
+      xOffset -= 20;
+    }
+    
+  }
+
+  displayRect();
+}
+
+function displayRect(){
+  for (let i = xOffset; i < width + xOffset; i++){
+    let thisRect = terrain[i];
+    rect(thisRect.x - xOffset, height - thisRect.height, 1, thisRect.height);
+  }
+}
+
+function spawnRectangles(){
+  let time = 0;
+  for (let x = 0; x < 10000; x++){
+    let h = noise(time)* height;
+    let thisRect = {
+      x: x,
+      height: h,
+    };
+    terrain.push(thisRect);
+    time += 0.003;
+
+  }
+}
+
+
--- index.md
+++ index.md
@@ -6,6 +6,7 @@
 -[Ball Array](05-ball-array)
 -[Perli Noise Ball](06-noise-ball)
 -[Sounds and images](08-sound)
+-[Terrain Generation](09-terrain)
 
 ## Projects
 - [Interactive scene](clicking-game)
